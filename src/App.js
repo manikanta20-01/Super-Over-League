b0b7dbcd3d70b0d2e04e65703fd651da553b02ce
Namo manikanta <122387885+manikanta20-01@users.noreply.github.com>
2023-02-27 11:19:05 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="League">
+      <h1 className="heading"> Super Over League</h1>
+      <div>
+        <img
+          className="img"
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        />
+        <img
+          className="img"
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        />
+      </div>
+    </div>
+  );
+}
